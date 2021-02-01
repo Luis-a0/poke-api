0c5fe037916ab00e8c4fd82fcd303385ecfdf071
Luis Aviles <lumaaviles0@gmail.com>
2021-02-01 15:03:14 -0500

    Se han agregado los componentes necesarios para realizar consultas a la web services

--- pokemon/pokedex/views.py
+++ pokemon/pokedex/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Pokemon
 
-# Create your views here.
+def json_pokemon(request, pokemon_name):
+    pokemon = Pokemon.objects.get(name=pokemon_name)
+
+    pokemones = Pokemon.objects.filter(chain_evol_id= pokemon.chain_evol_id).values()
+    pokemones = list(pokemones)
+    data = {'data': pokemones}
+
+    return JsonResponse(data)
--- pokemon/pokemon/urls.py
+++ pokemon/pokemon/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from pokedex import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^(?P<pokemon_name>(\w|\d|\s|\-|\.)+)/$', views.json_pokemon, name='json_pokemon'),
 ]
