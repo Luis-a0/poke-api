0caf1fd619db0e4219d3f5f74a4417329b46c07e
Luis Aviles <lumaaviles0@gmail.com>
2021-02-01 07:06:41 -0500

    Creacion de estructura de base de dato requerida.

--- pokemon/pokedex/models.py
+++ pokemon/pokedex/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+	id_pokemon = models.IntegerField(primary_key=True)
+	name = models.CharField(max_length=20)
+	height = models.FloatField()
+	weight = models.FloatField()
+	hp_base = models.IntegerField()
+	attack_base = models.IntegerField()
+	attack_sp_base = models.IntegerField()
+	defense_base = models.IntegerField()
+	defense_sp_base = models.IntegerField()
+	speed_base = models.IntegerField()
+	chain_evo_rel = models.ForeignKey('ChainEvol', db_column='chain_evol_rel', on_delete=models.PROTECT)
+
+	class Meta:
+		managed = True
+		db_table = 'pokemon'
+
+	def __str__(self):
+		return str(self.name)
+
+class ChainEvol(models.Model):
+	id_chain = models.IntegerField(primary_key=True)
+
+	class Meta:
+		managed = True
+		db_table = 'chain_evol'
